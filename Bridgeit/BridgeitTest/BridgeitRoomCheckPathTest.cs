69b72aed255826edcf52c42e07364d5aa34d2b9f
Mblkolo <rusakov-rv@yandex.ru>
2015-09-25 18:29:31 +0500

    Добавлены тесты вертикального поля

--- Bridgeit/BridgeitTest/BridgeitRoomCheckPathTest.cs
+++ Bridgeit/BridgeitTest/BridgeitRoomCheckPathTest.cs
@@ -8,38 +8,37 @@ namespace BridgeitTest
     public class BridgeitRoomCheckPathTest
     {
         [TestMethod]
-        public void OneCeilExistPath()
+        public void OneCeilNotExistPath()
+        {
+            var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 1 }, 1, 2);
+
+            var pathExist = BridgeitRoom.CheckHorizontalPath(room);
+
+            Assert.IsFalse(pathExist);
+        }
+
+        [TestMethod]
+        public void OneCeilExistHorizontalPath()
         {
             var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 1 }, 1, 2);
             room.Field[0, 0] = 1;
 
-            var pathExist = BridgeitRoom.CheckHorisonatalPath(room);
+            var pathExist = BridgeitRoom.CheckHorizontalPath(room);
 
             Assert.IsTrue(pathExist);
             Assert.AreEqual(room.Field[0, 0], 1);
         }
 
         [TestMethod]
-        public void OneCeilNotExistPath()
+        public void OneCeilExistVerticalPath()
         {
             var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 1 }, 1, 2);
+            room.Field[0, 0] = 2;
 
-            var pathExist = BridgeitRoom.CheckHorisonatalPath(room);
-
-            Assert.IsFalse(pathExist);
-        }
-
-        [TestMethod]
-        public void TwoCeilExistPath()
-        {
-            var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 2 }, 1, 2);
-            room.Field[2, 0] = 1;
-            room.Field[1, 1] = 1;
-            room.Field[0, 2] = 1;
-
-            var pathExist = BridgeitRoom.CheckHorisonatalPath(room);
+            var pathExist = BridgeitRoom.CheckVerticalPath(room);
 
             Assert.IsTrue(pathExist);
+            Assert.AreEqual(room.Field[0, 0], 2);
         }
 
         [TestMethod]
@@ -48,10 +47,50 @@ namespace BridgeitTest
             var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 2 }, 1, 2);
             room.Field[0, 0] = 1;
             room.Field[2, 2] = 1;
+            room.Field[0, 2] = 2;
+            room.Field[2, 0] = 2;
 
-            var pathExist = BridgeitRoom.CheckHorisonatalPath(room);
+            var hPathExist = BridgeitRoom.CheckHorizontalPath(room);
+            var vPathExist = BridgeitRoom.CheckVerticalPath(room);
 
-            Assert.IsFalse(pathExist);
+            Assert.IsFalse(hPathExist);
+            Assert.IsFalse(vPathExist);
+            Assert.AreEqual(room.Field[0, 0], 1);
+            Assert.AreEqual(room.Field[2, 2], 1);
+            Assert.AreEqual(room.Field[0, 2], 2);
+            Assert.AreEqual(room.Field[2, 0], 2);
+        }
+
+        [TestMethod]
+        public void TwoCeilHorizontalExistPath()
+        {
+            var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 2 }, 1, 2);
+            room.Field[2, 0] = 1;
+            room.Field[1, 1] = 1;
+            room.Field[0, 2] = 1;
+
+            var pathExist = BridgeitRoom.CheckHorizontalPath(room);
+
+            Assert.IsTrue(pathExist);
+            Assert.AreEqual(room.Field[2, 0], 1);
+            Assert.AreEqual(room.Field[1, 1], 1);
+            Assert.AreEqual(room.Field[0, 2], 1);
+        }
+
+        [TestMethod]
+        public void TwoCeilVerticalExistPath()
+        {
+            var room = new BridgeitRoom(1, new RoomSettings { Id = 1, Name = "Name", Size = 2 }, 1, 2);
+            room.Field[2, 0] = 2;
+            room.Field[1, 1] = 2;
+            room.Field[0, 2] = 2;
+
+            var pathExist = BridgeitRoom.CheckVerticalPath(room);
+
+            Assert.IsTrue(pathExist);
+            Assert.AreEqual(room.Field[2, 0], 2);
+            Assert.AreEqual(room.Field[1, 1], 2);
+            Assert.AreEqual(room.Field[0, 2], 2);
         }
 
         [TestMethod]
@@ -70,7 +109,7 @@ namespace BridgeitTest
             }
 
             for(int i=0; i<1000; ++i)
-                Assert.IsTrue(BridgeitRoom.CheckHorisonatalPath(room)); 
+                Assert.IsTrue(BridgeitRoom.CheckHorizontalPath(room)); 
         }
     }
 }
